385457c0ff62efb3dd39ab83b156d71008c3152c
Sean Cooper <scooper@adaptavist.com>
2025-03-29 00:36:43 +0000

    add comments

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from rapidfuzz.distance.Levenshtein import distance
 
 
 def extract_q_grams(word, q):
+    """
+    Returns a list of all contiguous q-grams of length q from the given word.
+    """
     q_grams = []
     for i in range(len(word) - q + 1):
         q_grams.append(word[i:i + q])
     return q_grams
 
 def extract_all_q_grams(words, q):
+    """
+    Returns a dictionary mapping each q-gram to the set of words that contain it.
+    """
     all_q_grams = {}
 
     for word in words:
@@ -20,6 +26,9 @@ def extract_all_q_grams(words, q):
     return all_q_grams
 
 def find_closet_matches(q_grams, word, q):
+    """
+    Returns a list of up to 5 words with the smallest Levenshtein distance to the input word, selected from the top 100 candidates based on shared q-grams.
+    """
     matches = {}
     input_q_grams = extract_q_grams(word, q)
     for q_gram in input_q_grams:
